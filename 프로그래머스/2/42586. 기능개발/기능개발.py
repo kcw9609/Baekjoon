499392f37e233c903a1e821b4d03f142ba9413ff
Chaewon Kang <101781313+kcw9609@users.noreply.github.com>
2026-03-19 12:37:07 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,28 +1,39 @@
-import math;
 def solution(progresses, speeds):
-    answer = {}
-
-
-    be_needDate = 0
+    answer = []
     
-    # 차례대로 필요한 일자 계산
-    for i in range(len(progresses)):
+    # exception
+    
+    def cul_dates(pr, speed):
+        count = 0
+        while pr < 100:
+            pr += speed
+            count += 1
+        return count
+    
+    # set
+    cnt = 1
+    max_date = cul_dates(progresses[0], speeds[0])
+    
+    # grogresses 만큼 반복:
+    for idx in range(1, len(progresses)):
+        curr_cnt = cul_dates(progresses[idx], speeds[idx])
         
-        # 걸리는 시간 계산
-        fo_remained = 100 - progresses[i]
-        fo_needDate = math.ceil(fo_remained / speeds[i])
-        
-        if fo_needDate > be_needDate:
-            answer[fo_needDate] = answer.get(fo_needDate, 0) + 1
-            be_needDate = fo_needDate # 새로운 기준일 업데이트
+        # if 앞에 일수 > 뒤의 일수:
+        if curr_cnt <= max_date:
+            # 카운트 추가
+            cnt += 1
+
+        # if 앞의 일수 < 뒤의 일수:
         else:
-            answer[be_needDate] = answer.get(be_needDate, 0) + 1
-            
-    return list(answer.values())
-
+            # 쌓인 업무들 배포
+            answer.append(cnt)
+            cnt = 1
+            max_date = curr_cnt
+            # 새롭게 카운트
+    answer.append(cnt)
         
-    
-    # 만약 앞의 작업의 시간이 더 오래걸리면 앞의 시간으로 배포
-    
-    # 만약 뒤의 시간이 더 걸리면 뒤의 시간으로 배포
-    
+    return answer
+
+
+
+# 7 3 9일 -> [2, 1]
