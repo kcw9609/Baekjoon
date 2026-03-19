49689da223052d6f325e5815132fd0a8da1679ca
Chaewon Kang <101781313+kcw9609@users.noreply.github.com>
2026-03-19 12:04:07 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 40.95 ms, Memory: 27.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    before = -1
+    
+    for i in arr:
+        if before != i:
+            answer.append(i)
+        before = i
+        
+    return answer
